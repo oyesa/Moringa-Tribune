a6ff49d2d4f1ad933155c2c92e0b98afa16d144e
oyesa <mercyoyesa2018@gmail.com>
2022-05-23 16:20:29 +0300

    initial commit

---
+++ news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('', views.welcome, name='welcome'),
+    path('^today/$',views.news_of_day,name='newsToday')
+    # path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')  
+]
