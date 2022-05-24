6f0d97e4f043eb38212bbc673547bcc33e6cf547
oyesa <mercyoyesa2018@gmail.com>
2022-05-24 08:31:26 +0300

    create Editor class in models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    class Meta:
+      ordering = ['first_name']
+
+
+
--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
     path('', views.welcome, name='welcome'),
     path('today/',views.news_of_day,name='newsToday'),
-    path('archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+    path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews')
 ]
