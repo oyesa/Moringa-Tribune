95456be8b6cdeb096dfef27c6ee6bc605f8040a7
oyesa <mercyoyesa2018@gmail.com>
2022-05-24 10:23:58 +0300

    create tests for model classes and update model class

--- news/models.py
+++ news/models.py
@@ -8,8 +8,24 @@ class Editor(models.Model):
 
     def __str__(self):
         return self.first_name
+
+    def save_editor(self):
+        self.save()
+        
     class Meta:
       ordering = ['first_name']
 
+class tags(models.Model):
+    name = models.CharField(max_length =30)
 
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey('Editor', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
