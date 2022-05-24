3d18f076d45603a8162a04607f385534be5e3bfb
oyesa <mercyoyesa2018@gmail.com>
2022-05-24 11:39:33 +0300

    create test class for articles

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.welcome, name='welcome'),
+    # path('', views.welcome, name='welcome'),
     path('today/',views.news_of_day,name='newsToday'),
     path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews')
 ]
