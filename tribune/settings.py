0da551c4690b24f16e4799fe42e4213e5242777c
oyesa <mercyoyesa2018@gmail.com>
2022-05-30 15:52:09 +0300

    create email templates and functions

--- tribune/settings.py
+++ tribune/settings.py
@@ -18,6 +18,13 @@ from decouple import config,Csv
 
 MODE=config("MODE", default="dev")
 
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
